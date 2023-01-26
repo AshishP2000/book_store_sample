6e67c47639548a05f23fabe4d6e43a0ce55af1cf
Ashish <ashish2000patil@gmail.com>
2023-01-26 08:16:04 +0530

    [Ashish]Added: Serializers

--- book_store/user/urls.py
+++ book_store/user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login')
+    path('user_register', views.UserRegister.as_view(), name='user_register'),
+    path('user_login', views.UserLogin.as_view(), name='user_login')
 ]
