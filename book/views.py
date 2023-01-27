b7edde105593e4c18d9f0e3702d863f76e3dc3af
Ashish <ashish2000patil@gmail.com>
2023-01-27 15:59:30 +0530

    [Ashish]Added: Book Crud Operations

--- book/views.py
+++ book/views.py
@@ -5,7 +5,7 @@ import logging
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from user.utils import verify_is_superuser, verify_user
 from .models import Book
 from .serializers import BookSerializer
 
@@ -14,6 +14,7 @@ logging.basicConfig(filename='book_store.log', level=logging.INFO)
 
 class BookAPI(APIView):
 
+    @verify_is_superuser
     def post(self, request):
         """
         post: getting title,author,price,quantity,created_at from user and saving in the database
@@ -25,21 +26,26 @@ class BookAPI(APIView):
             serializer.save()
             return Response({'message': 'Book is registered', 'status': 201, 'data': serializer.data},
                             status=status.HTTP_201_CREATED)
+        except ValueError as ex:
+            print(ex)
+        except AttributeError as ex:
+            print(ex)
         except Exception as ex:
             logging.exception(ex)
             return Response({'message': str(ex)})
 
+    @verify_user
     def get(self, request):
         try:
-            data = Book.objects.filter(id=request.data.get('id'))
+            data = Book.objects.all()
             serializer = BookSerializer(data, many=True)
-            print(serializer)
             return Response({'message': 'Book', 'status': 200, 'data': serializer.data},
                             status=status.HTTP_200_OK)
         except Exception as ex:
             logging.exception(ex)
             return Response({'message': str(ex)})
 
+    @verify_is_superuser
     def put(self, request):
         """
         put: getting title,author,price,quantity,updated_at from user and saving in the database
@@ -52,10 +58,15 @@ class BookAPI(APIView):
             serializer.save()
             return Response({'message': 'Data is updated', 'status': 200, 'data': serializer.data},
                             status=status.HTTP_200_OK)
+        except ValueError as ex:
+            print(ex)
+        except AttributeError as ex:
+            print(ex)
         except Exception as ex:
             logging.exception(ex)
             return Response({'message': str(ex)})
 
+    @verify_is_superuser
     def delete(self, request):
         """
         post: getting id from user and deleting that book from database
@@ -66,6 +77,10 @@ class BookAPI(APIView):
             book_data.delete()
             return Response({'message': 'Book deleted', 'status': 204},
                             status=status.HTTP_204_NO_CONTENT)
+        except ValueError as ex:
+            print(ex)
+        except AttributeError as ex:
+            print(ex)
         except Exception as ex:
             logging.exception(ex)
             return Response({'message': str(ex)})
--- book_store/user/models.py
+++
@@ -1,8 +0,0 @@
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    phone = models.BigIntegerField(default=0)
-    is_verify = models.BooleanField(default=False)
